3be7d832dbb363c3383cac6dee61263852206a0c
Lorenzo Bogdan <bogdan.lorenzo11@gmail.com>
2019-01-09 18:14:34 +0100

    First working Connection
    NEED FIX:
    -Initialize two connection instead of one
    
    TODO:
    -Json need to be implement

---
+++ Assets/Scripts/ConnectionToServer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using UnityEngine.Networking;
+
+public class ConnectionToServer : MonoBehaviour {
+
+    string url = "https://app-mobile-api.herokuapp.com/";
+    
+    private void Start()
+    {
+        string msg = "Hey claudiu";
+        using (UnityWebRequest www = UnityWebRequest.Post(url,msg))
+        {
+            Debug.Log(www.error);
+        }
+        
+    }
+}
