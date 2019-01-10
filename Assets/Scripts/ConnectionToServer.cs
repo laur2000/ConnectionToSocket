12a91b2683c0846bebf5da3a4ab94edc56656028
Lorenzo Bogdan <bogdan.lorenzo11@gmail.com>
2019-01-10 23:30:08 +0100

    -Added menu selection -Added Json data conversion -Added post request to http
    NEED TO FIX:
    -Server doesn't recognize string data as Json

--- Assets/Scripts/ConnectionToServer.cs
+++ Assets/Scripts/ConnectionToServer.cs
@@ -2,30 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 using WebSocketSharp;
+using UnityEngine.Networking;
 
+public class JsonData
+{
+    public string id;
+    public string name;
+    public string color;
 
+   public JsonData(string ID)
+    {
+        id = ID;
+    }
+    //Get the parametres of the JSON
+   public string GetId()
+    {
+        return id;
+    }
+    public string GetName()
+    {
+        return id;
+    }
+    public string GetColor()
+    {
+        return id;
+    }
+
+    //Set the parametres of the JSON
+    
+    public void SetName(string NAME)
+    {
+        name = NAME;
+    }
+    public void SetColor(string COLOR)
+    {
+        color = COLOR;
+    }
+}
 public class ConnectionToServer : MonoBehaviour
 {
     WebSocket ws;
+    string web_uri = "ws://app-mobile-api.herokuapp.com";
+    JsonData player;
 	// Use this for initialization
-	void Start () {
-         ws= new WebSocket("ws://app-mobile-api.herokuapp.com");
-        
+	public void StartConnection () {
+
+         ws= new WebSocket(web_uri);
+         bool is_first = true;
             ws.OnOpen += (sender, e) => {
                 Debug.Log("Connection established!");
             };
             ws.OnMessage += (sender, e) => {
+                if(is_first)
+                {
+                    player = new JsonData(e.Data.Substring(4));
+
+                    
+                    is_first = false;
+                }
                 Debug.Log(e.Data);
             };
             ws.Connect();
-
-            
-        
+        //string data =JsonConvert.SerializeObject(new JsonData());
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void SetDataJson(string name,string color)
+    {
+        player.SetName(name);
+        player.SetColor(color);
+    }
+    public void SendJsonData()
+    {
+        
+        StartCoroutine("Post");
+
+    }
+    
+   IEnumerator Post()
+    {
+        string data = JsonUtility.ToJson(player);
+        Debug.Log("Trying to upload: " + data);
+        using (UnityWebRequest www = UnityWebRequest.Post(web_uri, data))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
+    }
+    
+
+
+
 }
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+
+{
+    [SerializeField]
+    Text nickname_text;
+    string nickname;
+    string color;
+    GameObject manager;
+    ConnectionToServer connection;
+    public void SetNickname()
+    {
+        nickname = nickname_text.text;
+    }
+    public void SetColorRed()
+    {
+        color = ColorUtility.ToHtmlStringRGB(Color.red);
+        color = "#" + color.ToLower();
+    }
+    public void SetColorBlue()
+    {
+        color = ColorUtility.ToHtmlStringRGB(Color.blue);
+        color = "#" + color.ToLower();
+    }
+    public void SetColorYellow()
+    {
+        color = ColorUtility.ToHtmlStringRGB(Color.yellow);
+        color = "#" + color.ToLower();
+    }
+    public void SetColorGreen()
+    {
+        color = ColorUtility.ToHtmlStringRGB(Color.green);
+        color = "#" + color.ToLower();
+    }
+    public void StartGame()
+    {
+
+        connection.SetDataJson(nickname, color);
+        connection.SendJsonData();
+        
+
+    }
+    
+    private void Awake()
+    {
+        manager = new GameObject();
+        manager.name = "networkManager";
+        manager.AddComponent<ConnectionToServer>();
+        connection = manager.GetComponent<ConnectionToServer>();
+        connection.StartConnection();
+    }
+
+}
